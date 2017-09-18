d86fc6eb41c9c9630c409043396c1a6131211423
ricardoop <liuchao.ricardo@gmail.com>
2017-09-18 18:06:57 +0800

    try

--- first_version.py
+++ first_version.py
@@ -28,36 +28,43 @@ class age():
             except ValueError:
                 print "you inter wrong date,please inter again"
 
-    def countAge(self,Check,Brith):
-        diff_day = (Check - Brith).days
-        print diff_day        
-        if diff_day<=14:
-            das = 30 - int(diff_day)
-            print datetime.now() + timedelta(days=das)
-        if diff_day <= 45 and diff_day>14:
-            das = 60 - int(diff_day)
-            print datetime.now() + timedelta(days=das)
-        if diff_day <= 90 and diff_day>45:
-            das = 120 - int(diff_day)
-            print datetime.now() + timedelta(days=das)
-        if diff_day <= 150 and diff_day>90:
-            das = 180 - int(diff_day)
-            print datetime.now() + timedelta(days=das)
-        if diff_day <= 240 and diff_day>150:
-            das = 270 - int(diff_day)
-            print datetime.now() + timedelta(days=das)
-        if diff_day <= 300 and diff_day>240:
-            das = 365 - int(diff_day)
-            print datetime.now() + timedelta(days=das)
-        if diff_day <= 390 and diff_day>300:
-            das = 450 - int(diff_day)
-            print datetime.now() + timedelta(days=das)
-        if diff_day <= 480 and diff_day>390:
-            das = 540 - int(diff_day)
-            print datetime.now() + timedelta(days=das)
-        if diff_day <= 570 and diff_day>480:
-            das = 730 - int(diff_day)
-            print datetime.now() + timedelta(days=das)
+    def countAge(self,brithday,checkday):
+        while True:
+            diff_day = (checkday - brithday).days
+            print diff_day
+            if diff_day > 0 :
+                if diff_day<=14:
+                    das = 30 - int(diff_day)
+                    print datetime.now() + timedelta(days=das)
+                if diff_day <= 45 and diff_day>14:
+                    das = 60 - int(diff_day)
+                    print datetime.now() + timedelta(days=das)
+                if diff_day <= 90 and diff_day>45:
+                    das = 120 - int(diff_day)
+                    print datetime.now() + timedelta(days=das)
+                if diff_day <= 150 and diff_day>90:
+                    das = 180 - int(diff_day)
+                    print datetime.now() + timedelta(days=das)
+                if diff_day <= 240 and diff_day>150:
+                    das = 270 - int(diff_day)
+                    print datetime.now() + timedelta(days=das)
+                if diff_day <= 300 and diff_day>240:
+                    das = 365 - int(diff_day)
+                    print datetime.now() + timedelta(days=das)
+                if diff_day <= 390 and diff_day>300:
+                    das = 450 - int(diff_day)
+                    print datetime.now() + timedelta(days=das)
+                if diff_day <= 480 and diff_day>390:
+                    das = 540 - int(diff_day)
+                    print datetime.now() + timedelta(days=das)
+                if diff_day <= 570 and diff_day>480:
+                    das = 730 - int(diff_day)
+                    print datetime.now() + timedelta(days=das)
+                    break
+            else:
+                print "you enter wrong date"
+                isValidDate_Brith()
+                isValidDate_Check()
 
 p=age()
 brith_date = p.isValidDate_Brith()
